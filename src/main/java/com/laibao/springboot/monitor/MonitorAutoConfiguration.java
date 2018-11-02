aa783e10373cf4de8f52d3a55424213cfdc7e47a
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-11-02 10:55:00 +0800

    update pom.xml file info

---
+++ src/main/java/com/laibao/springboot/monitor/MonitorAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.laibao.springboot.monitor;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author  laibao wang
+ */
+
+@Configuration
+public class MonitorAutoConfiguration {
+}
